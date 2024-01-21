fa1c0159048e6f4bb52aa0bc4dfea74073163055
Gihozo23 <christellegihozo23@gmail.com>
2024-01-21 17:17:18 +0200

    new fields added for submission

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    //
+    public function showForm()
+    {
+        return view('form');
+    }
+
+    public function submitForm(Request $request)
+    {
+        // Validate form data
+        $validatedData = $request->validate([
+            'firstName' => 'required|string|max:30',
+            'lastName' => 'required|string|max:30',
+            'email' => 'required|email',
+            'phone-number'=> 'required|string|max:10',
+            'summary' => 'required|string|max:255',
+            'title' => 'required|string|max:50',
+
+        ]);
+
+        // Assuming the form submission is successful
+        // You can add your logic for saving data to the database here
+        $yourModel = new YourModel();
+        $yourModel->field1 = $validatedData['field1'];
+        $yourModel->field2 = $validatedData['field2'];
+        // Set other fields accordingly
+
+        $yourModel->save();
+
+        return redirect('/submit-form')->with('success', 'Content submitted successfully!');
+    }
 }
